3b043faccfdfd3abdfb223861cba4017ba25c671
Germeson Martins <germeson_martins@icloud.com>
2023-04-08 04:09:26 -0300

    First commit

---
+++ src/Program.java
@@ -0,0 +1,13 @@
+public class Program {
+    public static void main(String[] args){
+        Smartphone smartphone = new Smartphone();
+        smartphone.fabricator();
+        smartphone.model();
+        smartphone.display();
+        smartphone.color();
+        smartphone.storage();
+        smartphone.version();
+        smartphone.camera();
+        smartphone.video();
+    }
+}
---
+++ src/Smartphone.java
@@ -0,0 +1,33 @@
+public class Smartphone {
+    public void fabricator(){
+        System.out.println("Apple");
+    }
+
+    public void model(){
+        System.out.println("iPhone 13");
+    }
+
+    public void display(){
+        System.out.println("6,1");
+    }
+
+    public void color(){
+        System.out.println("Midnight");
+    }
+
+    public void storage(){
+        System.out.println("512GB");
+    }
+
+    public void version (){
+        System.out.println("IOS 15");
+    }
+
+    public void camera(){
+        System.out.println("12 Mpx");
+    }
+
+    public void video(){
+        System.out.println("4K");
+    }
+}
